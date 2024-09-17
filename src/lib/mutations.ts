f6283f8d8ceb09c1397d84a982408638c52005b1
efeguerrero <juanfg.8640@gmail.com>
2024-09-17 17:26:26 -0300

    setQueryData for Bookmark group orders items alphabetically

--- src/lib/mutations.ts
+++ src/lib/mutations.ts
@@ -33,7 +33,18 @@ export const useNewBookmarkGroup = () => {
         bookmarkGroupsQueryOptions.queryKey,
         (oldData) => {
           if (oldData) {
-            return [...oldData, data];
+            const newData = [...oldData, data];
+
+            newData.sort((a, b) => {
+              const nameA = a.name.toLowerCase();
+              const nameB = b.name.toLowerCase();
+
+              if (nameA < nameB) return -1;
+              if (nameA > nameB) return 1;
+              return 0;
+            });
+
+            return newData;
           }
           return [data];
         },
