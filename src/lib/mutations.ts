e87de88b76888a71fd6eda9331fea35172123560
efeguerrero <juanfg.8640@gmail.com>
2024-09-18 17:51:11 -0300

    updated group is now also sorted alphabetically

--- src/lib/mutations.ts
+++ src/lib/mutations.ts
@@ -120,6 +120,15 @@ export const useUpdateBookmarkGroup = () => {
               }
               return item;
             });
+
+            newData.sort((a, b) => {
+              const nameA = a.name.toLowerCase();
+              const nameB = b.name.toLowerCase();
+              if (nameA < nameB) return -1;
+              if (nameA > nameB) return +1;
+              return 0;
+            });
+
             return newData;
           }
 
