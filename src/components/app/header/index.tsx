766acd41906c1839c338cb40d9badb7ea7ecc42b
efeguerrero <juanfg.8640@gmail.com>
2024-10-03 13:41:27 -0300

    new xs screen of 450px

--- src/components/app/header/index.tsx
+++ src/components/app/header/index.tsx
@@ -4,9 +4,9 @@ import UserInfo from "./user-info";
 const Header = () => {
   return (
     <header className="flex flex-wrap items-center justify-between border-b p-4">
-      <div className="contents flex-wrap items-center min-[450px]:flex min-[450px]:space-x-4">
+      <div className="xs:flex xs:space-x-4 contents flex-wrap items-center">
         <h1 className="text-2xl font-semibold">B</h1>
-        <GroupsMenu className="order-3 mt-5 w-full max-w-md min-[450px]:mt-0 min-[450px]:w-[200px]" />
+        <GroupsMenu className="xs:mt-0 xs:w-[200px] order-3 mt-5 w-full max-w-md" />
       </div>
       <UserInfo />
     </header>
