877e99d4cb70e3dff1830e5609d2fa21031e998d
efeguerrero <juanfg.8640@gmail.com>
2024-10-03 17:34:57 -0300

    whatever

--- src/components/app/header/index.tsx
+++ src/components/app/header/index.tsx
@@ -4,9 +4,9 @@ import UserInfo from "./user-info";
 const Header = () => {
   return (
     <header className="flex flex-wrap items-center justify-between border-b p-4">
-      <div className="xs:flex xs:space-x-4 contents flex-wrap items-center">
+      <div className="contents flex-wrap items-center xs:flex xs:space-x-4">
         <h1 className="text-2xl font-semibold">B</h1>
-        <GroupsMenu className="xs:mt-0 xs:w-[250px] order-3 mt-5 w-full max-w-md" />
+        <GroupsMenu className="order-3 mt-5 w-full max-w-md xs:mt-0 xs:w-[250px]" />
       </div>
       <UserInfo />
     </header>
