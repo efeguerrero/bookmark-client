7942ab7b7bb5c32cd8fdcfc0f8a6040780bc14eb
efeguerrero <juanfg.8640@gmail.com>
2024-10-04 14:55:21 -0300

    header is now fully responsive

--- src/components/app/header/index.tsx
+++ src/components/app/header/index.tsx
@@ -4,9 +4,9 @@ import UserInfo from "./user-info";
 const Header = () => {
   return (
     <header className="fixed inset-x-0 top-0 z-10 flex h-[--nav-height] w-full items-center justify-between gap-x-8 border-b bg-background p-4">
-      <div className="flex items-center space-x-4">
-        <h1 className="text-2xl font-semibold">B</h1>
-        <GroupsMenu className="" />
+      <div className="flex items-center justify-start gap-x-4">
+        <h1 className="shrink-0 text-2xl font-semibold">B</h1>
+        <GroupsMenu className="w-[250px] shrink" />
       </div>
       <UserInfo />
     </header>
