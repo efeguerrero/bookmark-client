9cd803bc99476ff7cc386902e0ffcc03c654b42e
efeguerrero <juanfg.8640@gmail.com>
2024-10-07 17:07:18 -0300

    icon in header is now link to /app

--- src/components/app/header/index.tsx
+++ src/components/app/header/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "@tanstack/react-router";
 import GroupsMenu from "./groups-menu";
 import UserInfo from "./user-info";
 import * as Icon from "@/components/ui/icons";
@@ -6,7 +7,9 @@ const Header = () => {
   return (
     <header className="fixed inset-x-0 top-0 z-10 flex h-[--nav-height] w-full items-center justify-between gap-x-8 border-b bg-background p-4">
       <div className="flex items-center justify-start gap-x-2">
-        <Icon.logo aria-label="Bookmarks logo" className="size-9 shrink-0" />
+        <Link to="/app">
+          <Icon.logo aria-label="Bookmarks logo" className="size-9 shrink-0" />
+        </Link>
         <GroupsMenu className="w-[250px] shrink" />
       </div>
       <UserInfo />
