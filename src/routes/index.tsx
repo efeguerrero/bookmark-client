7d44906b14a4ba71294884e067ffa21cd087a7ef
efeguerrero <juanfg.8640@gmail.com>
2024-10-07 16:19:27 -0300

    homepage done

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
+import Home from "@/components/home";
 
 export const Route = createFileRoute("/")({
   component: Home,
 });
-
-function Home() {
-  return (
-    <div>
-      <h2 className="text-3xl font-medium tracking-tight">Bookmarker</h2>
-    </div>
-  );
-}
